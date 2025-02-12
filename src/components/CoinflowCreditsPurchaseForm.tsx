c8c5b81d4468786ac333ca33222e5e73cb3bd487
Benjamin Meeder <ben@coinflowlabs.app>
2025-02-12 08:52:02 -0600

    Env vars and staging env

--- src/components/CoinflowCreditsPurchaseForm.tsx
+++ src/components/CoinflowCreditsPurchaseForm.tsx
@@ -1,9 +1,9 @@
 export function CoinflowCreditsPurchaseForm({sellerId, subtotal}: {sellerId: string, subtotal: {cents: number}}) {
-  const url = `http://localhost:3003/checkout-credits?cents=${subtotal.cents}&sellerId=${sellerId}`;
+  const url = `${import.meta.env.VITE_LANDING_URL}/checkout-credits?cents=${subtotal.cents}&sellerId=${sellerId}&feePercentage=10`;
 
   return (
     <div className={"h-full flex-1 w-full relative"}>
-      <div style={{height: `${450}px`, minHeight: `${450}px`, width: '350px'}}
+      <div style={{height: `${450}px`, width: '350px'}}
            className={
              "flex-col h-full flex mx-auto relative overflow-hidden rounded-none md:rounded-xl md:border border-black/5"
            }
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,2 @@
-export const MERCHANT_ID = 'zero-day-demo';
-export const API_KEY = 'coinflow_sandbox_f7566868ac974f2883c4b4f850a1f6c1_0248cf93c2754d76a8f9da20966932d5';
-export const BASE_URL = 'http://localhost:5000';
+export const MERCHANT_ID = import.meta.env.VITE_MERCHANT_ID;
+export const API_KEY = import.meta.env.VITE_API_KEY;
